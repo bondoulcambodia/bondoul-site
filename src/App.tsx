538d3c7ac13eaca5990cccbed10ad22a1a3dfc04
maneth909 <hoeunmaneth09@gmail.com>
2025-08-01 15:48:31 +0700

    deploy fixed

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/bondoul-website">
+      <BrowserRouter basename="/bondoul-site">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
