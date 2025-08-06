68322b79d12f5884d97b2266810cd84e07d7219d
maneth909 <hoeunmaneth09@gmail.com>
2025-08-06 14:56:55 +0700

    og meta tags added

--- src/components/SubscriptionHero.tsx
+++ src/components/SubscriptionHero.tsx
@@ -12,7 +12,7 @@ const SubscriptionHero = () => {
     <section className="relative min-h-screen w-full grid grid-cols-1 lg:grid-cols-2 bg-gradient-hero p-10 md:p-20">
       <div className="flex flex-col items-center lg:items-start justify-center">
         <h1 className="text-center lg:text-left text-4xl font-bold tracking-tighter sm:text-5xl md:text-6xl lg:text-7xl/none">
-          Find Your Dream Job, Faster
+          A Partner For Your <span className="text-primary">Career</span>
         </h1>
         <p className="text-center lg:text-left max-w-[700px] text-muted-foreground md:text-xl mt-4">
           Our platform connects you with top companies actively looking for
